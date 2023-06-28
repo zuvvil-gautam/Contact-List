acbbdad7ff9d911983b2a351070e88aa88c27e6e
zuvvil <zuvvilgautam007@gmail.com>
2023-06-28 12:02:21 +0530

    Rendered ejs file

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const port = 8000;
+
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
 app.get('/', function(req,res){
-    res.send('<h1>Yo, response from my first express server!!</h1>');
+
+    return res.render('home');
 });
 
 
