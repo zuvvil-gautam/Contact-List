f0d645b14de8d87f8c0ad8e974d08cc5dd25892a
zuvvil <zuvvilgautam007@gmail.com>
2023-06-29 15:59:19 +0530

    Rendering contact list by passing the array contact_list through res.render in index.js file

--- index.js
+++ index.js
@@ -8,10 +8,51 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
+
+var contactList = [
+    {
+        name:'Luffy',
+        phone:'123'
+    },
+    {
+        name:'Zuvvil',
+        phone:'234'
+    },
+    {
+        name:'Gautam',
+        phone:'345'
+    },
+    {
+        name:'Zoro',
+        phone:'456'
+    },
+    {
+        name:'Chopper',
+        phone:'567'
+    },
+    {
+        name:'Nami',
+        phone:'678'
+    },
+    {
+        name:'Sanji',
+        phone:'789'
+    }
+]
+
+
 app.get('/', function(req,res){
 
-    return res.render('home', {title: "My contact list"});
+    return res.render('home', {
+        title: "Contact List",
+        contact_list:contactList
+    });
 });
+
+app.get('/play', function(req,res){
+
+    return res.render('practise', {title:"Playground"})
+})
 
 
 app.listen(port, function(err){
